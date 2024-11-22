0481d2b78af325adcbbc1ffd38c685b0576fb18c
MicheleGrav <150924862+MicheleGrav@users.noreply.github.com>
2024-11-22 12:07:21 +0100

    Update section ID

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Home = () => {
     return (
-        <div className='flex justify-center pt-5 pb-5 animate-fadeIn'>
+        <div id="home" className='flex justify-center pt-5 pb-5 animate-fadeIn'>
             <div className="flex flex-wrap justify-between h-[100%] w-[80%] bg-[rgba(255,255,255,0.2)] dark:bg-[rgba(255,255,255,0.59)] backdrop-blur-lg shadow-lg shadow-black/50 rounded-3xl p-10">
                 <div className='group animate-fadeIn delay-300 flex flex-col gap-5 mr-5 ml-5'>
                     <img src={userLogo} alt="user logo" className='h-[200px] dark:invert dark:brightness-0 dark:contrast-200 transition-all duration-300'></img>
