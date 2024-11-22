8431371f2db4c91ab1ed4aa28d6d932dfa2fb42c
MicheleGrav <150924862+MicheleGrav@users.noreply.github.com>
2024-11-22 11:26:15 +0100

    Update navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,27 +11,27 @@ const Navbar = () => {
                 <ul className="flex items-center justify-between gap-8 mx-auto max-w-full">
                     <li className="cursor-pointer text-lg font-semibold text-gray-800 transition ease-in-out delay-75 bg-inherit hover:bg-slate500 duration300">
                         <a href="#home" className="text-lg font-semibold text-gray-800 uppercase tracking-wide relative inline-block p-3 
-                        after:content-[''] after:absolute after:bottom-0 after:right-[10%] after:w-[80%] after:h-[2px] after:bg-black
+                        after:content-[''] after:absolute after:bottom-0 after:right-[5%] after:w-[90%] after:h-[2px] after:bg-black
                         after:transition-all after:duration-300 after:ease-in-out
-                        hover:after:w-0 hover:after:left-[10%]">Home</a>
+                        hover:after:w-0 hover:after:left-[5%]">Home</a>
                     </li>
-                    <li className="text-lg font-semibold text-gray-800 uppercase tracking-wide relative inline-block p-3 
-                        after:content-[''] after:absolute after:bottom-0 after:right-[10%] after:w-[80%] after:h-[2px] after:bg-black
+                    <li className="cursor-pointer text-lg font-semibold text-gray-800 transition ease-in-out delay-75 bg-inherit hover:bg-slate500 duration300">
+                        <a href="#stack" className="text-lg font-semibold text-gray-800 uppercase tracking-wide relative inline-block p-3 
+                        after:content-[''] after:absolute after:bottom-0 after:right-[5%] after:w-[90%] after:h-[2px] after:bg-black
                         after:transition-all after:duration-300 after:ease-in-out
-                        hover:after:w-0 hover:after:left-[10%]">
-                        <a href="#stack" className="p-3 rounded-xl hover:animate-pulse">Tech Stack</a>
+                        hover:after:w-0 hover:after:left-[5%] hover:animate-pulse">Tech Stack</a>
                     </li>
-                    <li className="text-lg font-semibold text-gray-800 uppercase tracking-wide relative inline-block p-3 
-                        after:content-[''] after:absolute after:bottom-0 after:right-[10%] after:w-[80%] after:h-[2px] after:bg-black
+                    <li className="cursor-pointer text-lg font-semibold text-gray-800 transition ease-in-out delay-75 bg-inherit hover:bg-slate500 duration300">
+                        <a href="#projects" className="text-lg font-semibold text-gray-800 uppercase tracking-wide relative inline-block p-3 
+                        after:content-[''] after:absolute after:bottom-0 after:right-[5%] after:w-[90%] after:h-[2px] after:bg-black
                         after:transition-all after:duration-300 after:ease-in-out
-                        hover:after:w-0 hover:after:left-[10%]">
-                        <a href="#projects" className="p-3 rounded-xl hover:animate-pulse">Gallery / Edu</a>
+                        hover:after:w-0 hover:after:left-[5%] hover:animate-pulse">Gallery / Edu</a>
                     </li>
-                    <li className="text-lg font-semibold text-gray-800 uppercase tracking-wide relative inline-block p-3 
-                        after:content-[''] after:absolute after:bottom-0 after:right-[10%] after:w-[80%] after:h-[2px] after:bg-black
+                    <li className="cursor-pointer text-lg font-semibold text-gray-800 transition ease-in-out delay-75 bg-inherit hover:bg-slate500 duration300">
+                        <a href="#contact" className="text-lg font-semibold text-gray-800 uppercase tracking-wide relative inline-block p-3 
+                        after:content-[''] after:absolute after:bottom-0 after:right-[5%] after:w-[90%] after:h-[2px] after:bg-black
                         after:transition-all after:duration-300 after:ease-in-out
-                        hover:after:w-0 hover:after:left-[10%]">
-                        <a href="#contact" className="p-3 rounded-xl hover:animate-pulse">Contact</a>
+                        hover:after:w-0 hover:after:left-[5%] hover:animate-pulse">Contact</a>
                     </li>
                 </ul>
             </div>
