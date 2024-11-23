61226e8f911561a0b1b660cf459f06a6776a2282
MicheleGrav <150924862+MicheleGrav@users.noreply.github.com>
2024-11-23 08:00:31 +0100

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import ThemeToggleButton from "./ThemeToggleButton";
 
 const Navbar = () => {
     return (
-        <nav className="mb-10 flex justify-between items-center p-3 bg-[rgba(255,255,255,0.2)] dark:bg-[rgba(63,63,63,0.51)] backdrop-blur-lg shadow-lg">
+        <nav id="navbar" className="mb-10 flex justify-between items-center p-3 bg-[rgba(255,255,255,0.2)] dark:bg-[rgba(63,63,63,0.51)] backdrop-blur-lg shadow-lg">
             {/* <div className="flex items-center">
                 <DiTerminalBadge className="h-11 w-11 hover:animate-pulse"/> 
             </div>*/}
@@ -14,12 +14,6 @@ const Navbar = () => {
 
             <div>
                 <ul className="flex items-center justify-between gap-8 mx-auto max-w-full">
-                    <li className="cursor-pointer text-lg font-semibold text-black transition ease-in-out delay-75 bg-inherit hover:bg-slate500 duration300">
-                        <a href="#home" className="text-lg font-semibold text-black dark:text-white uppercase tracking-wide relative inline-block p-3 
-                        after:content-[''] after:absolute after:bottom-0 after:right-[5%] after:w-[90%] after:h-[2px] after:bg-black dark:after:bg-white transition-colors duration-700 ease-in-out
-                        after:transition-all after:duration-300 after:ease-in-out
-                        hover:after:w-0 hover:after:left-[5%]">Home</a>
-                    </li>
                     <li className="cursor-pointer text-lg font-semibold text-black transition ease-in-out delay-75 bg-inherit hover:bg-slate500 duration300">
                         <a href="#stack" className="text-lg font-semibold text-black dark:text-white uppercase tracking-wide relative inline-block p-3 
                         after:content-[''] after:absolute after:bottom-0 after:right-[5%] after:w-[90%] after:h-[2px] after:bg-black dark:after:bg-white transition-colors duration-700 ease-in-out
