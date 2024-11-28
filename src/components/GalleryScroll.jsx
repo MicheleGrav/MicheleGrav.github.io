77ad4650cebb0d6d97d103e422f261061366f547
MicheleGrav <150924862+MicheleGrav@users.noreply.github.com>
2024-11-28 15:16:47 +0100

    Update Gallery with hover animation component

--- src/components/GalleryScroll.jsx
+++ src/components/GalleryScroll.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import { useScroll, useTransform } from "framer-motion";
 import { motion } from "framer-motion";
 import PropTypes from "prop-types";
+import { PinContainer } from "./PinContainer";  // import PinContainer
 
 const GalleryScroll = ({ images, className }) => {
   const containerRef = useRef(null); // For future custom container usage
@@ -17,44 +18,54 @@ const GalleryScroll = ({ images, className }) => {
   const secondPart = images.slice(third, 2 * third);
   const thirdPart = images.slice(2 * third);
 
-  const backgroundHeight = useTransform(scrollYProgress, [0, 1], ["100%", "0%"]);
-
   return (
     <div
       className={`mt-10 place-self-center items-start w-full ${className} overflow-y-hidden bg-[rgba(255,255,255,0.2)] dark:bg-[rgba(63,63,63,0.51)] backdrop-blur-lg shadow-lg shadow-black/50 rounded-3xl max-w-[80%] max-h-[200vh]`}
       ref={containerRef}
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start max-w-5xl mx-auto gap-10 py-40 px-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start max-w-5xl mx-auto gap-5 py-40 px-0">
         <div className="grid gap-10">
           {firstPart.map((el, idx) => (
             <motion.div style={{ y: translateFirst }} key={`grid-1-${idx}`}>
-              <img
-                src={el}
-                className="h-80 w-full object-cover object-left-top rounded-lg gap-10 m-0 p-0"
-                alt="thumbnail"
-              />
+              <PinContainer href={el} title="Image Title" className="h-80 w-full">
+                <div className="flex basis-full flex-col p-4 tracking-tight sm:basis-1/2 w-[20rem] h-[20rem]">
+                  <img
+                    src={el}
+                    className="h-full w-full object-cover object-center rounded-lg"
+                    alt={`thumbnail-${idx}`}
+                  />
+                </div>
+              </PinContainer>
             </motion.div>
           ))}
         </div>
         <div className="grid gap-10">
           {secondPart.map((el, idx) => (
             <motion.div style={{ y: translateSecond }} key={`grid-2-${idx}`}>
-              <img
-                src={el}
-                className="h-80 w-full object-cover object-left-top rounded-lg gap-10 m-0 p-0"
-                alt="thumbnail"
-              />
+              <PinContainer href={el} title="Image Title" className="h-80 w-full">
+                <div className="flex basis-full flex-col p-4 tracking-tight sm:basis-1/2 w-[20rem] h-[20rem]">
+                  <img
+                    src={el}
+                    className="h-full w-full object-cover object-center rounded-lg"
+                    alt={`thumbnail-${idx}`}
+                  />
+                </div>
+              </PinContainer>
             </motion.div>
           ))}
         </div>
         <div className="grid gap-10">
           {thirdPart.map((el, idx) => (
             <motion.div style={{ y: translateThird }} key={`grid-3-${idx}`}>
-              <img
-                src={el}
-                className="h-80 w-full object-cover object-left-top rounded-lg gap-10 m-0 p-0"
-                alt="thumbnail"
-              />
+              <PinContainer href={el} title="Column 3" className="h-80 w-full">
+              <div className="flex basis-full flex-col p-4 tracking-tight sm:basis-1/2 w-[20rem] h-[20rem]">
+                  <img
+                    src={el}
+                    className="h-full w-full object-cover object-center rounded-lg"
+                    alt={`thumbnail-${idx}`}
+                  />
+                </div>
+              </PinContainer>
             </motion.div>
           ))}
         </div>
