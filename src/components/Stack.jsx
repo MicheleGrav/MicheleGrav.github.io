e5d9a8a6c193c38d68d85f2de682c0e279f75042
MicheleGrav <150924862+MicheleGrav@users.noreply.github.com>
2024-12-01 21:51:59 +0100

    Update Stack.jsx

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -2,8 +2,8 @@ const Stack = () => {
     return (
         <div id="stack" className="flex justify-around mt-10 mb-10">
             <div className="flex flex-col dark:text-white transition-all duration-300 gap-5">
-                <h2 className="text-3xl">Frameworks</h2>
-                <ul>
+                <h2 className="text-xl">Frameworks</h2>
+                <ul className="text-sm">
                     <li>React</li>
                     <li>TailwindCSS</li>
                     <li>Flutter</li>
@@ -12,8 +12,8 @@ const Stack = () => {
             </div>
 
             <div className="flex flex-col dark:text-white transition-all duration-300 gap-5">
-                <h2 className="text-3xl ">Languages</h2>
-                <ul>
+                <h2 className="text-xl ">Languages</h2>
+                <ul className="text-sm">
                     <li>HTML</li>
                     <li>CSS</li>
                     <li>JavaScript</li>
